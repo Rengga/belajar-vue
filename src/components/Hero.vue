a43fed15473ed29a8fbe8fc164e9707a08d40ecb
ngidolMaki <renggaarnanta@gmail.com>
2021-10-15 00:54:25 +0700

    belajar vue menu awal

---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <div class="col align-self-center">
+      <h2>
+        <strong>Menu Enak Banget, </strong><br />
+        didalam aplikasi mu
+      </h2>
+      <p>ayo pilih makananmu sekarang dan rasakan nikmatnya</p>
+      <button class="btn btn-dark border-0" style="background-color: #6C63FF">Pesan</button>
+    </div>
+    <div class="col">
+      <img src="../assets/images/draw.svg" width="80%" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+};
+</script>
+
+<style></style>
---
+++ src/views/Foods.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navbar />
+    <h2>panganan</h2>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  name: 'Foods',
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style></style>
